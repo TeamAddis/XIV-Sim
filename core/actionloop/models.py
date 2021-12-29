3a4229d233a6e2c243d53103fe033d64af42885d
Andrew Addis <addisandrew1@gmail.com>
2021-12-29 23:39:48 +0900

    updated simulator and loop to calculate potency of actions

--- core/actionloop/models.py
+++ core/actionloop/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 
 class ActionLoop:
-    actions = []
+    
+    def __init__(self, id):
+        self.actions = []
+        self.id = id
+
+
+    def add_action(self, action):
+        self.actions.append(action)
+
+    def calculate_loop_potency(self):
+        potency = 0
+        for action in self.actions:
+            if action.potency:
+                potency = potency + action.potency
+        return potency
--- core/simulator/models.py
+++ core/simulator/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Simulator:
+    
+    def __init__(self):
+        self.action_loops = []
+
+    def add_action_loop(self, action_loop):
+        self.action_loops.append(action_loop)
+
+    def calculate_total_potency(self):
+        potency = 0
+        for action_loop in self.action_loops:
+            potency = potency + action_loop.calculate_loop_potency()
+
+    
